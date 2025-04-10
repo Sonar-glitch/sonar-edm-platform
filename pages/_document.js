f6e8edfbf044459b93eb7bdda2510c1127387927
Sonar-glitch <furqanz.email@gmail.com>
2025-04-10 00:50:34 -0400

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -5,14 +5,17 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Montserrat:wght@400;700&family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Inter:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet" />
+          <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    ) ;
+    );
   }
 }
 
